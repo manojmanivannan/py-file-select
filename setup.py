acf774d9995394157ba0e2bb575b7ff2917bf988
Manoj Manivannan <manojm18@live.in>
2025-10-01 18:56:02 +0100

    update req

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='py_file_select',
     version='0.1',
     packages=find_packages(),
-    install_requires=['inquirer==3.4.0','click==8.1.7'],
+    install_requires=['inquirer','click'],
     entry_points={
         'console_scripts': [
             'py_file_select=source.py_file_select:main',
